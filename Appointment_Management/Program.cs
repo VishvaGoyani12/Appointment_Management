0f60d5c28f0f765275a36d2b03e31549d856d00d
VishvaGoyani12 <mt.vishva.goyani@gmail.com>
2025-05-26 15:44:11 +0530

    fix_error

--- Appointment_Management/Program.cs
+++ Appointment_Management/Program.cs
@@ -89,9 +89,6 @@ builder.Services.AddAuthentication(options =>
 });
 
 
-
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myconndb")));
@@ -105,7 +102,6 @@ using (var scope = app.Services.CreateScope())
     await DbSeeder.SeedRolesAndAdminAsync(services);
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
